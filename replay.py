e0eac7ee78935420b2c70b5616463decc94aa8b0
Connor Monahan <cma2714@gmail.com>
2021-03-30 22:58:08 -0500

    whoops

--- replay.py
+++ replay.py
@@ -11,8 +11,15 @@ parser = argparse.ArgumentParser(description='Replay vision')
 parser.add_argument('file')
 args = parser.parse_args()
 v = vision.Vision(record=False)
+v.frame_skip = 0
 q = queue.Queue(maxsize=1)
 v.open_input(args.file)
 status = ""
 while True:
     v.check_new_frame(q, status)
+    try:
+        data = q.get(block=False)
+        if data['type'] == 'quit':
+            break
+    except queue.Empty:
+        pass
